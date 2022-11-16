be02f9c376f16d4bdb0ac44539b94369cb202727
lukiina42 <lukas.vala25@gmail.com>
2022-11-16 22:35:25 +0100

    Load db config from env file

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -6,21 +6,24 @@ import { User } from './user/user.entity';
 import { HighScores } from './highScores/highScores.entity'
 import { UsersModule } from './user/users.module';
 import { HighScoresModule } from './highScores/highScores.module';
-import { HighScoresController } from './highScores/highScores.controller';
-import { HighScoresService } from './highScores/highScores.service';
-import { UsersService } from './user/users.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 6000,
-    username: 'apiserver',
-    password: '2412',
-    database: 'postgres',
-    entities: [User, HighScores],
-    synchronize: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], 
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres' as 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: parseInt(configService.get<string>('DB_PORT')),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities:  [User, HighScores],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
   }),
   UsersModule,
   HighScoresModule
